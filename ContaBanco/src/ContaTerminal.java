2821f48b9d965774e0ebc8855d093eda638a4c8a
PaulaDaniele <pauladfds@gmail.com>
2024-08-16 13:36:14 -0300

    Exercício proposto ContaBanco.

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+           
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número de sua agência: ");
+        String Agencia = sc.nextLine();
+        System.out.println();
+
+        System.out.print("Agora digite o número de sua conta: ");
+        String numeroContaStr = sc.nextLine();
+        int Numero = Integer.parseInt(numeroContaStr);
+        System.out.println();
+
+        System.out.print("Informe seu nome completo: ");
+        String NomeCliente = sc.nextLine();
+        System.out.println();
+
+        System.out.print("Informe o valor que deseja sacar: ");
+        String saldoStr = sc.nextLine();
+        double Saldo = Double.parseDouble(saldoStr);
+
+        sc.close();
+
+        DadosCliente cliente = new DadosCliente(Numero, Agencia, NomeCliente, Saldo);
+
+        System.out.println(cliente.toString());
+    }
+}
